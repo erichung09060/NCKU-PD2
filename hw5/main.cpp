2e38db980871905d2c8222ce21d99a268388160f
Eric <101823195+erichung1113@users.noreply.github.com>
2023-05-13 02:16:16 +0800

    hw5

--- hw5/main.cpp
+++ hw5/main.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 struct node {
     node *nxt[26] = {NULL};
-    set<int> idx;
+    unordered_set<int> id;
 };
 
 int main(int argc, char **argv) {
+    //cin.sync_with_stdio(0),cin.tie(0);
     node *root = new node;
 
     FILE *file = fopen(argv[1], "r");
@@ -16,15 +17,14 @@ int main(int argc, char **argv) {
     char line[100005];
 
     // Read Data
-    set<int> all_idx;
     while (fgets(line, sizeof(line), file)) {
-        int idx = 0, i = 0, quote = 0;
-        while (isdigit(line[i])) idx = idx * 10 + line[i++] - '0';
-        all_idx.insert(idx);
+        int id = 0, i = 0, quote = 0;
+        while (isdigit(line[i])) id = id * 10 + line[i++] - '0';
+
         node *p = root;
         for (; line[i] != 10 && line[i] != 13; i++) {
             if (line[i] == ' ' && p != root) {
-                p->idx.insert(idx);
+                p->id.insert(id);
                 p = root;
             } else if (isalpha(line[i])) {
                 int to = tolower(line[i]) - 'a';
@@ -32,35 +32,43 @@ int main(int argc, char **argv) {
                 p = p->nxt[to];
             }
         }
-        p->idx.insert(idx);
+        p->id.insert(id);
     }
     // Query
     while (fgets(line, sizeof(line), file2)) {
         node *p = root;
-        set<int> ans = all_idx;
-        int n = strlen(line);
+        unordered_map<int,int> cnt;
+        int n = strlen(line), words = 0;
         line[n++] = 10;
-        for (int i = 0; i < n; i++) {
-            bool fail_match = false;
-            if (line[i] == ' ' || line[i] == 10 || line[i] == 13) {
-                set<int> tmp;
-                set_intersection(all(ans), all(p->idx), inserter(tmp, tmp.begin()));
-                ans = tmp;
+        bool fail = false;
+        for (int i = 0; !fail && line[i] != 10 && line[i] != 13; i++) {
+            if (line[i] == ' ' && p != root) {
+                for(auto i:p->id) cnt[i]++;
+                words++;
                 p = root;
             } else if (isalpha(line[i])) {
                 int to = tolower(line[i]) - 'a';
                 if (p->nxt[to]) p = p->nxt[to];
-                else fail_match = true;
+                else fail = true;
             } else {
-                fail_match = true;
+                fail = true;
             }
-            if (fail_match) {
-                ans.clear();
-                break;
-            }
-            if (line[i] == 10 || line[i] == 13) break;
         }
-        if (ans.empty()) cout << -1 << '\n';
-        else for (auto &idx : ans) cout << idx << " \n"[idx == *prev(ans.end())];
+        
+        if(p != root){
+            for(auto i:p->id) cnt[i]++;
+            words++;
+        }
+        
+        if(fail){
+            cout << -1 << '\n';
+        }else{
+            vector<int> ans;
+            for(auto i:cnt) if(i.second == words) ans.pb(i.first);
+            sort(all(ans));
+
+            if (ans.empty()) cout << -1 << '\n';
+            else for (auto &i : ans) cout << i << " \n"[i == ans.back()];
+        }
     }
 }
