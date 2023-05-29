35c060ce572a1605201710ec031a09348101ce3a
Eric <101823195+erichung1113@users.noreply.github.com>
2023-05-29 21:04:13 +0800

    hw6

--- hw6/hw6val.sh
+++ hw6/hw6val.sh
@@ -1,4 +1,4 @@
-if ! test -d ~/hw6/result ; then mkdir ~/hw6/result; fi
+if ! test -d ~/hw6/results ; then mkdir ~/hw6/results; fi
 if ! test -d ~/hw6/answer ; then mkdir ~/hw6/answer; fi
 cp -rf /home/share/hw6/* ~/hw6/answer
 make -k clean all -C ~/hw6
@@ -10,13 +10,13 @@ for i in {1..5}; do
 		corpus=~/hw6/answer/testcase$i/corpus$i
 		query=~/hw6/answer/testcase$i/query$i
 		answer=~/hw6/answer/testcase${i}/result_corpus${i}_query${i}_${k}
-		result=~/hw6/result/result${i}_${k}
+		result=~/hw6/results/result${i}_${k}
 		echo -n "testcase $i, k = $k : "
 		duration=$( { /usr/bin/time -f "%E" ./hw6 $corpus $query $k > $result; } 2>&1) 
 		diff $result $answer >> /dev/null
 		if (( $? != 0 )); then
 			echo "WA, time(real) : $duration"
-			echo "diff ./result/result${i}_${k} ./answer/testcase${i}/result_corpus${i}_query${i}_${k}"
+			echo "diff ./results/result${i}_${k} ./answer/testcase${i}/result_corpus${i}_query${i}_${k}"
 		else
 			echo "AC, time(real) : $duration"
 		fi
